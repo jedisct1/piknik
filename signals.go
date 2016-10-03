56f7372b68966bc89b69588f016918e87028f6b2
Frank Denis <github@pureftpd.org>
2016-10-03 15:21:22 +0200

    sighandlers: use standard exit codes, use SIGINFO to print info
    Also: interruptions shouldn't kill the server, and print how long
    ago the clipboard was last filled.

--- signals.go
+++ signals.go
@@ -3,32 +3,49 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func handleSignals() {
 	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGUSR1)
-
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINFO)
 	for {
 		select {
 		case signal, ok := <-signals:
 			if !ok {
 				break
 			}
-
 			switch signal {
-			case syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM:
-				os.Exit(2)
-			case syscall.SIGUSR1:
-				if len(storedContent.ciphertextWithNonce) > 0 {
-					fmt.Printf("%v: some data is stored\n", os.Args[0])
-				} else {
-					fmt.Printf("%v: no data stored yet\n", os.Args[0])
+			case syscall.SIGINT:
+				os.Exit(128 + int(syscall.SIGINT))
+			case syscall.SIGQUIT:
+				os.Exit(128 + int(syscall.SIGQUIT))
+			case syscall.SIGTERM:
+				os.Exit(128 + int(syscall.SIGTERM))
+			case syscall.SIGINFO:
+				storedContent.RLock()
+				procName := "piknik"
+				if len(os.Args) >= 1 {
+					procName = os.Args[0]
 				}
+				if storedContent.ts == nil {
+					fmt.Printf("%v: the clipboard is empty\n", procName)
+				} else {
+					elapsed := time.Since(time.Unix(int64(binary.LittleEndian.Uint64(storedContent.ts)), 0))
+					if elapsed <= 1 {
+						fmt.Printf("%v: the clipboard is not empty (last filled a few moments ago)\n",
+							procName)
+					} else {
+						fmt.Printf("%v: the clipboard is not empty (last filled %v minutes ago)\n",
+							procName, elapsed)
+					}
+				}
+				storedContent.RUnlock()
 			}
 		}
 	}
--- signals_windows.go
+++ signals_windows.go
@@ -9,18 +9,16 @@ import (
 
 func handleSignals() {
 	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt, os.Kill)
-
+	signal.Notify(signals, os.Kill)
 	for {
 		select {
 		case signal, ok := <-signals:
 			if !ok {
 				break
 			}
-
 			switch signal {
-			case os.Interrupt, os.Kill:
-				os.Exit(2)
+			case os.Kill:
+				os.Exit(128 + 15)
 			}
 		}
 	}
