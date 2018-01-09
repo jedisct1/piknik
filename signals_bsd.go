5b52a1c192f3eb6fb7080ead5bfb38a1c7049b26
Frank Denis <github@pureftpd.org>
2018-01-09 08:22:54 +0100

    megacheck

--- signals_bsd.go
+++ signals_bsd.go
@@ -15,32 +15,30 @@ func handleSignals() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINFO)
 	for {
-		select {
-		case signal, ok := <-signals:
-			if !ok {
-				break
+		signal, ok := <-signals
+		if !ok {
+			break
+		}
+		switch signal {
+		case syscall.SIGINFO:
+			storedContent.RLock()
+			procName := "piknik"
+			if len(os.Args) >= 1 {
+				procName = os.Args[0]
 			}
-			switch signal {
-			case syscall.SIGINFO:
-				storedContent.RLock()
-				procName := "piknik"
-				if len(os.Args) >= 1 {
-					procName = os.Args[0]
-				}
-				if storedContent.ts == nil {
-					fmt.Printf("%v: the clipboard is empty\n", procName)
+			if storedContent.ts == nil {
+				fmt.Printf("%v: the clipboard is empty\n", procName)
+			} else {
+				elapsed := time.Since(time.Unix(int64(binary.LittleEndian.Uint64(storedContent.ts)), 0))
+				if elapsed <= 1 {
+					fmt.Printf("%v: the clipboard is not empty (last filled a few moments ago)\n",
+						procName)
 				} else {
-					elapsed := time.Since(time.Unix(int64(binary.LittleEndian.Uint64(storedContent.ts)), 0))
-					if elapsed <= 1 {
-						fmt.Printf("%v: the clipboard is not empty (last filled a few moments ago)\n",
-							procName)
-					} else {
-						fmt.Printf("%v: the clipboard is not empty (last filled %v minutes ago)\n",
-							procName, elapsed)
-					}
+					fmt.Printf("%v: the clipboard is not empty (last filled %v minutes ago)\n",
+						procName, elapsed)
 				}
-				storedContent.RUnlock()
 			}
+			storedContent.RUnlock()
 		}
 	}
 }
