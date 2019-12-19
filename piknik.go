2abb3d8381fcc259cc7950a2ec7816e607d8ef26
Frank Denis <github@pureftpd.org>
2019-12-19 17:09:06 +0100

    Bump major

--- piknik.go
+++ piknik.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	// Version - Piknik version
-	Version = "0.9.1"
+	Version = "0.10.0"
 	// DomainStr - BLAKE2 domain (personalization)
 	DomainStr = "PK"
 	// DefaultListen - Default value for the Listen parameter
